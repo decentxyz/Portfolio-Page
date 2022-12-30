5e5d6e434de21c25e3a53921d5ed4f3bcb95346a
Charlie <85839774+cdsiren@users.noreply.github.com>
2022-12-29 20:34:41 -0500

    Update MarketplaceButtons.tsx

--- components/MarketplaceButtons.tsx
+++ components/MarketplaceButtons.tsx
@@ -15,6 +15,9 @@ const MarketplaceButtons = (props:any) => {
 
   return <div className="flex gap-4 items-center mt-4 w-full justify-center">
     <p className="font-[500] tracking-widest text-sm">View on:</p>
+    {/* <Link href={`https://opensea.io/collection/${props.openseaLink}`} target="_blank" rel="noreferrer">
+    <Image src="/images/opensea.png" height={18} width={18} alt="opensea"/></Link> */}
+    {/* this repo currently only supports Decent's editions contract */}
     <Link href={`https://hq.decent.xyz/${chain}/Editions/${props.contractAddress}`} target="_blank" rel="noreferrer">
     <Image src="/images/decent-icon.png" height={18} width={22} alt="decent"/></Link>
   </div>
